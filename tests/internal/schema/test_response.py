bb25e880c47ed41a901dc632f6879e2012e95fb2
axiangcoding <axiangcoding@gmail.com>
2023-12-18 16:44:54 +0800

    Add test cases for CommonResult class in test_response.py

--- tests/internal/schema/test_response.py
+++ tests/internal/schema/test_response.py
@@ -1,13 +1,27 @@
+from typing import Any
+
+import pytest
+
+from robot33.internal.schema.errors import CommonError
 from robot33.internal.schema.response import CommonResult
 
 
-def test_common_result_success():
-    cr = CommonResult.success()
+@pytest.mark.parametrize("data", [{"data": "01"}, ("data", 2), "str only", 0.3, 4])
+def test_common_result_success(data: object):
+    cr = CommonResult.success(data)
     assert cr.code == 0
     assert cr.message == "success"
+    assert cr.data == data
 
 
-def test_common_result_error():
-    cr = CommonResult.error()
-    assert cr.code == 1000
-    assert cr.message == "internal error"
+@pytest.mark.parametrize("ce,message", [
+    (CommonError.ERROR, None),
+    (CommonError.ERROR, "custom error"),
+])
+def test_common_result_error(ce: CommonError, message: str):
+    cr = CommonResult.error(ce, message)
+    assert cr.code == ce.code
+    if message:
+        assert cr.message == message
+    else:
+        assert cr.message == ce.description
