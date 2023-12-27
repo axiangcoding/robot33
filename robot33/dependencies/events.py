347bd5a82b4c1a0faf9a771fe3eb8279d9b33943
axiangcoding <axiangcoding@gmail.com>
2023-12-27 16:47:48 +0800

    Refactor health endpoint and remove unused code

--- robot33/dependencies/events.py
+++ robot33/dependencies/events.py
@@ -5,8 +5,3 @@ from robot33.internal.db import db
 
 async def startup_event():
     logger.info("Starting up...")
-
-
-async def __check_db_status__() -> bool:
-    ret = await db.database.command("ping")
-    return ret["ok"] == 1
--- tests/routers/test_root.py
+++ tests/routers/test_root.py
@@ -5,4 +5,8 @@ def test_get_health(test_client: TestClient):
     with test_client:
         response = test_client.get("/health")
         assert response.status_code == 200
-        assert response.json() == {"code": 0, "data": {"status": "ok"}, "message": "success"}
+        resp = response.json()
+        assert resp["code"] == 0
+        assert resp["message"] == "success"
+        assert resp["data"]["app_status"] == "ok"
+        assert resp["data"]["db_status"] == "ok"
