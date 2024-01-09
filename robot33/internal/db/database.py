789ebe971ad7abd356b824c47cf2222cfd37f9ad
axiangcoding <axiangcoding@gmail.com>
2024-01-09 09:46:00 +0800

    Update dependencies and refactor code

--- robot33/internal/db/database.py
+++ robot33/internal/db/database.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+from pydantic import BaseModel
 from pymongo import MongoClient
 
 from robot33 import config
@@ -7,3 +9,25 @@ client = MongoClient(config.get_settings().db.mongodb_url)
 database = client.get_database(config.get_settings().db.mongodb_database)
 
 document_collection = database.get_collection(DOCUMENT_COLLECTION_NAME)
+
+
+class DBDAO(ABC, BaseModel):
+    @abstractmethod
+    def insert_one(self, data: BaseModel) -> str:
+        pass
+
+    @abstractmethod
+    def delete_one(self, id: str) -> int:
+        pass
+
+    @abstractmethod
+    def update_one(self, id: str, data: BaseModel) -> int:
+        pass
+
+    @abstractmethod
+    def find_one(self, id: str) -> BaseModel:
+        pass
+
+    @abstractmethod
+    def find_all(self) -> list:
+        pass
